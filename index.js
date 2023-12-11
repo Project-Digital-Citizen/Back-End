b53f81868f4df8f44150013bbf560b8c2c6a4c06
WestleeMa <115126072+WestleeMa@users.noreply.github.com>
2023-12-12 01:19:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,98 +1,91 @@
-require('dotenv').config()
-const express = require('express');
-const fs = require('fs');
-const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const express = require("express");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const emailRoutes = require('./routes/emailRoutes');
-const ktpRoutes = require('./routes/ktpRoutes');
-const statusRoutes = require('./routes/statusRoutes');
-const {
-    swaggerUi,
-    specs
-} = require('./swagger');
-const {
-    connectDatabase
-} = require('./db/database');
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const emailRoutes = require("./routes/emailRoutes");
+const ktpRoutes = require("./routes/ktpRoutes");
+const statusRoutes = require("./routes/statusRoutes");
+const { swaggerUi, specs } = require("./swagger");
+const { connectDatabase } = require("./db/database");
 
 const app = express();
 
-app.options('*', cors());
-app.use(cors('*'));
+app.options("*", cors());
+app.use(cors("*"));
 app.use(express.json());
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(express.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // Connect to MongoDB
 connectDatabase();
 
-app.get('/images/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const imagePath = getImagePath(filename);
+app.get("/images/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const imagePath = getImagePath(filename);
 
-    if (imagePath) {
-        res.set('Content-Type', 'image/jpeg');
-        res.sendFile(imagePath);
-    } else {
-        res.status(404).send('Image not found');
-    }
+  if (imagePath) {
+    res.set("Content-Type", "image/jpeg");
+    res.sendFile(imagePath);
+  } else {
+    res.status(404).send("Image not found");
+  }
 });
 
 function getImagePath(filename) {
-    const imagesPath = path.join(__dirname, 'public', 'images');
+  const imagesPath = path.join(__dirname, "public", "images");
 
-    // Recursively scan directories and subdirectories
-    function scanDirectories(directory) {
-        const files = fs.readdirSync(directory);
+  // Recursively scan directories and subdirectories
+  function scanDirectories(directory) {
+    const files = fs.readdirSync(directory);
 
-        for (const file of files) {
-            const filePath = path.join(directory, file);
+    for (const file of files) {
+      const filePath = path.join(directory, file);
 
-            // Check if it's a directory
-            if (fs.statSync(filePath).isDirectory()) {
-                // Recursively scan the subdirectory
-                const subdirectoryPath = scanDirectories(filePath);
+      // Check if it's a directory
+      if (fs.statSync(filePath).isDirectory()) {
+        // Recursively scan the subdirectory
+        const subdirectoryPath = scanDirectories(filePath);
 
-                // If the file is found in the subdirectory, return its path
-                if (subdirectoryPath) {
-                    return subdirectoryPath;
-                }
-            } else if (file === filename) {
-                // If the file is found in the current directory, return its path
-                return filePath;
-            }
+        // If the file is found in the subdirectory, return its path
+        if (subdirectoryPath) {
+          return subdirectoryPath;
         }
-
-        // If the file is not found in this directory or its subdirectories
-        return null;
+      } else if (file === filename) {
+        // If the file is found in the current directory, return its path
+        return filePath;
+      }
     }
 
-    return scanDirectories(imagesPath);
+    // If the file is not found in this directory or its subdirectories
+    return null;
+  }
+
+  return scanDirectories(imagesPath);
 }
 
-
-
 // Home route
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Halo, selamat datang di Digzen!'
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Halo, selamat datang di Digzen!",
+  });
 });
 
 // Serve Swagger UI
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Use auth routes
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-app.use('/email', emailRoutes);
-app.use('/ktp', ktpRoutes);
-app.use('/status', statusRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/email", emailRoutes);
+app.use("/ktp", ktpRoutes);
+app.use("/status", statusRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
