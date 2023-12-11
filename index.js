c451a81f1e4d9a7753821cd7578ed14a897f1622
WestleeMa <115126072+WestleeMa@users.noreply.github.com>
2023-12-12 02:02:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const { connectDatabase } = require("./db/database");
 const app = express();
 
 app.options("*", cors());
-app.use(cors("*"));
+// app.use(cors("*"));
+app.use(cors({ origin: "https://api.digzen.site", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
