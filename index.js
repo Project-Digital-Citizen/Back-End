4b8ba7b12d7bb419e60971db2b759c356665385b
KINGBOTs1 <ds767365@gmail.com>
2023-12-14 02:34:09 +0800

    Revert "Revert "Update index.js""
    This reverts commit 65886701b6e73b03d9343ad0996efcd2c6d992e5.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { connectDatabase } = require("./db/database");
 const app = express();
 
 app.options("*", cors());
-app.use(cors("*"));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
