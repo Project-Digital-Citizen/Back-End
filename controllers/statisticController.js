935a52570a417b1fc2b0a7b92852fca5e58c4c33
KINGBOTs1 <ds767365@gmail.com>
2023-12-18 01:03:12 +0800

    add cron jobs for create automatic statistic on database

--- controllers/statisticController.js
+++ controllers/statisticController.js
@@ -1,6 +1,33 @@
 // controllers/statisticController.js
 const Statistic = require('../models/Statistic');
+const cron = require('cron');
 
+// Function to update statistics daily
+async function updateStatisticsDaily() {
+    try {
+        // Add your logic to fetch and update statistics here
+        // For example, you can query the database for the data you need
+        const userRegisterCount = 0
+        const userMailingCount = 0
+
+        // Get the current date
+        const currentDate = new Date().toISOString().split('T')[0];
+
+        const newStatistic = new Statistic({
+            userRegistrations: userRegisterCount,
+            userMailings: userMailingCount,
+            date: currentDate
+        });
+        await newStatistic.save();
+
+        console.log(`Statistics updated for ${currentDate}`);
+    } catch (error) {
+        console.error('Error updating statistics:', error.message);
+    }
+}
+
+// Schedule the job to run daily at midnight using cron
+new cron.CronJob('0 0 * * *', updateStatisticsDaily, null, true, 'UTC');
 async function getAllStatistics(req, res) {
     try {
         const statistics = await Statistic.find();
